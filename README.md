689eeeafd09d5584eadd1f28e7991551c1a0992c
Mourad <mouradensit@gmail.com>
2021-12-31 18:04:03 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# track-covid19
+ 
---
+++ src/app/components/country-picker/country-picker.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { CovidTrackerService } from 'src/app/covid-tracker.service';
+import { SharedCountryService } from '../../services/shared-country.service';
+
+interface CountryView {
+  value: string;
+  viewValue: string;
+}
+
+@Component({
+  selector: 'app-country-picker',
+  templateUrl: './country-picker.component.html',
+  styleUrls: ['./country-picker.component.scss']
+})
+
+export class CountryPickerComponent implements OnInit {
+
+  selectedCountry: string = 'Global';
+  countryView: CountryView[] = [];
+  public overAllData;
+  @Input() public darkTheme;
+
+  constructor(private data: CovidTrackerService, private _sharedService: SharedCountryService) {
+    this._sharedService.dataSource.subscribe((data) => {
+      this.overAllData = data;
+    });
+  }
+
+  sendMessage(selectedCountry): void {
+    this._sharedService.countrySource.next(selectedCountry);
+  }
+
+  getCountries = () => {
+    this.countryView.push({
+      value: 'Global',
+      viewValue: 'Global'
+    });
+  }
+
+  ngOnInit(): void {
+    this.getCountries();
+    this.sendMessage(this.selectedCountry);
+  }
+
+}
